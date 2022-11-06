e6aee54a314a3afcb16f9a593cdb8275f35d56a2
Jocaagura <oficinameltablero@gmail.com>
2022-11-06 13:01:05 -0500

    first json decode problem

--- lib/packages.dart
+++ lib/packages.dart
@@ -1,3 +1,13 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:packages/const.dart';
+
+Future<void> executeExercise() async {
+  final response = await http.get(Uri.parse(kUrl));
+
+  final Map<String, dynamic> jsonDecoded = jsonDecode(response.body);
+  print(jsonDecoded);
+  print('hola');
+  return;
 }
